2e23d33ff3d6a0b063647845229ace23521e26ad
Pablo Ventura <pablo@claude.workgroup>
2017-10-05 13:09:15 -0300

    agregando operaciones

--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@
 #!/usr/bin/env python
 
 from models import RelationalModel
-from relations import Relation
+from relops import Relation, Operation
 
 class ParserError(Exception):
     """
@@ -18,33 +18,54 @@ def stdin_parser():
     """
     linenumber = 1
     relations = {}
+    operations = {}
     try:
         universe = map(int,input().split()) # first line, universe
         linenumber += 1
         assert input() == "", ("Line #%s must be empty"%linenumber)
         linenumber += 1
         while True:
-            # parsing relations
-            # format:
+            # parsing operations and relations
+            # operations format:
+            #   symbol arity
+            #   (arity+1)-uples
+            #   empty line
+            # relations format:
             #   symbol number_of_tuples arity
             #   tuples
             #   empty line
             try:
-                sym, ntuples, arity = input().split()
+                ro_line = input().split()
                 linenumber += 1
             except EOFError:
                 # no more relations found
                 break
-
-            ntuples,arity = int(ntuples),int(arity)
-            relation = Relation(sym,arity)
-            for i in range(ntuples):
-                relation.add(tuple(map(int,input().split())))
+            if len(ro_line) == 3:
+                # parsing relation
+                sym, ntuples, arity = ro_line
+                ntuples,arity = int(ntuples),int(arity)
+                relation = Relation(sym,arity)
+                for i in range(ntuples):
+                    relation.add(tuple(map(int,input().split())))
+                    linenumber += 1
+                assert input() == "", ("Rel/Op must finish with empty line at #%s line"%linenumber) # relation MUST finish with empty line
                 linenumber += 1
-            assert input() == "", ("Relation must finish with empty line at #%s line"%linenumber) # relation MUST finish with empty line
-            linenumber += 1
-            relations[sym] = relation
+                relations[sym] = relation
+            elif len(ro_line) == 2:
+                # parsing operation
+                sym, arity = ro_line
+                arity = int(arity)
+                operation = Operation(sym,arity)
+                ntuples = len(universe)**arity
+                for i in range(ntuples):
+                    operation.add(tuple(map(int,input().split())))
+                    linenumber += 1
+                assert input() == "", ("Rel/Op must finish with empty line at #%s line"%linenumber) # relation MUST finish with empty line
+                linenumber += 1
+                operations[sym] = operation
+            else:
+                raise ParserError(linenumber,"Unexpected line")
     except EOFError:
         raise ParserError(linenumber,"Unexpected EOF")
     
-    return RelationalModel(universe,relations)
+    return Model(universe,relations,operations)
--- relops.py
+++ relops.py
@@ -39,3 +39,41 @@ class Relation(object):
             if set(t) <= subuniverse:
                 result.add(t)
         return result        
+
+class Operation(object):
+    """
+    Operation
+    """
+    def __init__(self,sym,arity):
+        self.sym = sym
+        self.arity = arity
+        self.op = dict()
+    
+    def add(self, t):
+        self.op[t[:-1]]=t[-1]
+    
+    def __repr__(self):
+        return repr(self.op)
+    
+    def __call__(self, *args):
+        return self.op[args]
+
+    def __len__(self):
+        return len(self.op)
+        
+    #def __iter__(self):
+    #    return iter(self.r)
+        
+    #def spectrum(self):
+    #    result = set()
+    #    for t in self:
+    #        result.add(len(set(t)))
+    #    return result
+    
+    def restrict(self,subuniverse):
+        result = Operation(self.sym,self.arity)
+        subuniverse= set(subuniverse)
+        for t in self.op:
+            if set(t) <= subuniverse:
+                result.add(t+(self.op[t],))
+        return result        
