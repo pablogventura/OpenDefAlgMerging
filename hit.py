811a3a4969fc062f1f75f639ea94b1f7f2c4f1c2
Pablo Ventura <pablo@claude.workgroup>
2017-12-28 11:24:18 -0300

    hit

--- hit.py
+++ hit.py
@@ -6,35 +6,10 @@ def tuple_number(t,order):
     return tuple(flath.index(e) for e in t)
 
 
-def tipoSubviejo(alpha, Func, debug=False):
-    # input alpha, Func
+def hit(model, alpha):
+    # input model, alpha
     alpha = list(sorted(alpha))
-    H = [alpha]
-    i = len(alpha)
-    T = defaultdict(set, {a: {i} for i, a in enumerate(alpha)})
-    O = H[-1]
-
-    while O:
-        H.append([])
-        for ar in sorted(Func):
-            for tup in TupAd(H, ar):
-                for sym,f in sorted(Func[ar],key=lambda f:f[0]):
-                    x = f(*tup)
-                    if any(x in h for h in H):
-                        T[x].add(i)
-                    else:
-                        T[x].add(i)
-                        H[-1].append(x)
-                    i += 1  # faltaba en el algoritmo original
-        O = H[-1]
-    if debug:
-        print (tuple(len(h) for h in H), sorted(tuple(len(T[k]) for k in sorted(T.keys()))))
-    else:
-        return H, T
-
-def tipoSub(alpha, Func, debug=False):
-    # input alpha, Func
-    alpha = list(sorted(alpha))
+    ops = model.operations
     H = [alpha]
     i = len(alpha)
     T = defaultdict(set, {a:{(len(H)-1,-1,(i,))}  for i, a in enumerate(alpha)})
@@ -42,10 +17,10 @@ def tipoSub(alpha, Func, debug=False):
 
     while O:
         H.append([])
-        for ar in sorted(Func):
+        for ar in sorted(ops):
             for tup in TupAd(H, ar):
-                i += 1  # faltaba en el algoritmo original
-                for sym_i,f in enumerate(sorted(Func[ar],key=lambda f:f[0])):
+                i += 1  
+                for sym_i,f in enumerate(sorted(ops[ar],key=lambda f:f[0])):
                     f=f[1]
                     x = f(*tup)
                     if any(x in h for h in H):
@@ -55,10 +30,7 @@ def tipoSub(alpha, Func, debug=False):
                         H[-1].append(x)
             i = 0
         O = H[-1]
-    if debug:
-        print (tuple(len(h) for h in H), sorted(tuple(len(T[k]) for k in sorted(T.keys()))))
-    else:
-        return H, T
+    return H, T
 
 
 def TupAd(h, k):
@@ -73,12 +45,3 @@ def TupAd(h, k):
         if any(t in o for t in tup):
             yield tup
 
-
-def join(a, b):
-    if a == 1 or b == 1:
-        return 1
-    elif a == 0:
-        return b
-    elif b == 0:
-        return a
-    return 1
