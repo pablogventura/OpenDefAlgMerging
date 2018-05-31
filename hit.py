8f8651b7b9b25e8bda8f2a0b37d15940a60e88ae
Pablo Ventura <pablo@claude.workgroup>
2018-05-31 12:14:34 -0300

    mejoro con sigma

--- hit.py
+++ hit.py
@@ -30,40 +30,43 @@ class TupleModelHash(object):
     """
     Clase de HIT, toma un modelo ambiente y la tupla generadora
     """
-    def __init__(self,model, generators):
+    def __init__(self,model, generators, THV = None):
         # input model, generators
         generators = list(generators)
         self.generators = generators
         self.model = model
-        ops = model.operations
-        ops =defaultdict(set)
-        for op in model.operations:
-            ops[model.operations[op].arity].add(model.operations[op])
-        
-        self.H = [generators]
-        i = len(generators)-1
-        self.T = defaultdict(set, {a:{i}  for i, a in enumerate(generators)})
-        O = self.H[-1]
-        self.V=list(generators)
-
-        while O:
-            self.H.append([])
-            for ar in sorted(ops):
+        if THV:
+            self.T,self.H,self.V = THV
+        else:
+            ops = model.operations
+            ops =defaultdict(set)
+            for op in model.operations:
+                ops[model.operations[op].arity].add(model.operations[op])
             
-                flath = [item for sublist in self.H for item in sublist]
-                o = self.H[-2]
-                for tup in product(flath, repeat=ar):
-                    i += 1
-                    if any(t in o for t in tup):
-                        for sym_i,f in enumerate(sorted(ops[ar],key=lambda f: f.sym)):
-                            x = f(*tup)
-                            self.V.append(x)
-                            self.T[x].add(i)
-                            if all(x not in h for h in self.H):
-                                self.H[-1].append(x)
+            self.H = [generators]
+            i = len(generators)-1
+            self.T = defaultdict(set, {a:{i}  for i, a in enumerate(generators)})
             O = self.H[-1]
-        self.H.pop()
-        self.T = {k:frozenset(self.T[k]) for k in self.T}
+            self.V=list(generators)
+
+            while O:
+                self.H.append([])
+                for ar in sorted(ops):
+                
+                    flath = [item for sublist in self.H for item in sublist]
+                    o = self.H[-2]
+                    for tup in product(flath, repeat=ar):
+                        i += 1
+                        if any(t in o for t in tup):
+                            for sym_i,f in enumerate(sorted(ops[ar],key=lambda f: f.sym)):
+                                x = f(*tup)
+                                self.V.append(x)
+                                self.T[x].add(i)
+                                if all(x not in h for h in self.H):
+                                    self.H[-1].append(x)
+                O = self.H[-1]
+            self.H.pop()
+            self.T = {k:frozenset(self.T[k]) for k in self.T}
     def __eq__(self,other):
         return set(self.T.values()) == set(other.T.values())
     
@@ -93,22 +96,22 @@ class TupleModelHash(object):
         result += ")"
         return result
     
+    
     def hit_p(self,perm):
-        TT = defaultdict(set)
+        sigma = list(perm)
         n = len(perm)
-        for b in range(1,len(self.H)+1):
+        for b in range(len(self.H)):
             for op in self.model.operations:
                 n+=n**self.model.operations[op].arity
                 b_1 = len(perm) # final del bloque anterior
                 for i in range(b_1,n):
                     s_i = int2base(i-b_1,len(self.generators),self.model.operations[op].arity)
-                    s_i = [perm[x] for x in s_i]
+                    s_i = [sigma[x] for x in s_i]
                     s_i = base2int(s_i,len(self.generators)) + b_1
                     perm.append(s_i)
+        VV = [perm[i] for i in self.V]
+        
         return perm
-
-
-
 
 if __name__ == "__main__":
     from parser import parser
