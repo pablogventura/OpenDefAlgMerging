7835c644eca911a1bb78279656c98511880285d9
Pablo Ventura <pablo@claude.WORKGROUP>
2018-10-22 11:20:27 -0300

    autopep8

--- hit.py
+++ hit.py
@@ -1,21 +1,31 @@
+# -*- coding: utf-8 -*-
+
+#!/usr/bin/env python
+
+"""
+Modulo para calcular HIT de una tupla en un modelo
+"""
+
 from itertools import product
 from collections import defaultdict
 from misc import indent
 from isomorphisms import Isomorphism
 
-d = 0
 
-
-def int2base(x, base, size=None):
-    valor = x
-    assert x >= 0
-    if x == 0:
+def int2base(number, base, size=None):
+    """
+    Convierte al numero x en base "base"
+    como una lista de largo size de enteros.
+    """
+    valor = number
+    assert number >= 0
+    if number == 0:
         digits = [0]
     else:
         digits = []
-        while x:
-            digits.append(x % base)
-            x = int(x / base)
+        while number:
+            digits.append(number % base)
+            number = int(number / base)
         digits.reverse()
     if size:
         if size < len(digits):
@@ -26,9 +36,14 @@ def int2base(x, base, size=None):
     return digits
 
 
-def base2int(x, base):
+def base2int(l_number, base):
+    """
+    Convierte una lista de numeros del 0 a base-1
+    en un numero entero considerando que es una lista
+    de simbolos en base "base"
+    """
     result = 0
-    for i, value in enumerate(reversed(x)):
+    for i, value in enumerate(reversed(l_number)):
         result += value*base**i
     return result
 
@@ -37,24 +52,34 @@ def permute(l, perm):
     return [l[perm[i]] for i in range(len(l))]
 
 
-class TupleModelHash(object):
+class TupleModelHash():
     """
     Clase de HIT, toma un modelo ambiente y la tupla generadora
     """
 
-    def __init__(self, model, generators):
-        # input model, generators
-        generators = list(generators)
-        self.generators = generators
+    def __init__(self, model, generator_tuple, th=None):
+        """
+        Calcula HIT de una tupla generadora en un modelo.
+        Si viene en th una tupla (T,H), se considera que son
+        los datos para un hit ya calculado (ie hitp)
+        """
+        generator_tuple = list(generator_tuple)
+        self.generator_tuple = generator_tuple
         self.model = model
+
+        if th:
+            # es un hit creado a partir de datos ya listos
+            self.T, self.H = th
+            return
 
         ops = defaultdict(set)
         for op in model.operations:
             ops[model.operations[op].arity].add(model.operations[op])
 
-        self.H = [generators]
-        i = len(generators)-1
-        self.T = defaultdict(set, {a: {j} for j, a in enumerate(generators)})
+        self.H = [generator_tuple]
+        i = len(generator_tuple)-1
+        self.T = defaultdict(set, {a: {j}
+                                   for j, a in enumerate(generator_tuple)})
         O = self.H[-1]
 
         while O:
@@ -62,11 +87,10 @@ class TupleModelHash(object):
             flath = [item for sublist in self.H for item in sublist]
             self.H.append([])
             for ar in sorted(ops):
-                for sym_i, f in enumerate(sorted(ops[ar], key=lambda f: f.sym)):
+                for f in sorted(ops[ar], key=lambda f: f.sym):
                     for tup in product(flath, repeat=ar):
                         i += 1
                         if any(t in O for t in tup):
-
                             x = f(*tup)
                             self.T[x].add(i)
                             if all(x not in h for h in self.H):
@@ -83,25 +107,26 @@ class TupleModelHash(object):
 
     def iso(self, other):
         if self == other:
-            flatSelfh = [item for sublist in self.H for item in sublist]
-            flatOtherh = [item for sublist in other.H for item in sublist]
-            d = {(flatSelfh[i]): flatOtherh[i] for i in range(len(flatSelfh))}
-            return Isomorphism(d, self.model.restrict(self.universe()), other.model.restrict(other.universe()), None)
-        else:
-            return None
+            flat_h_self = [item for sublist in self.H for item in sublist]
+            flat_h_other = [item for sublist in other.H for item in sublist]
+            d = {(flat_h_self[i]): flat_h_other[i]
+                 for i in range(len(flat_h_self))}
+            return Isomorphism(d, self.model.restrict(self.universe()),
+                               other.model.restrict(other.universe()), None)
+        return None
 
     def tuple(self):
-        return self.generators
+        return self.generator_tuple
 
     def universe(self):
-        return set([item for sublist in self.H for item in sublist])
+        return {item for sublist in self.H for item in sublist}
 
     def structure(self):
         return self.model.restrict(self.universe())
 
     def __repr__(self):
         result = "TupleModelHash(\n"
-        result += indent("Tuple=%s,\n" % self.generators)
+        result += indent("Tuple=%s,\n" % self.generator_tuple)
         result += indent("History=%s,\n" % self.H)
         result += indent("Type=%s,\n" % dict(self.T))
         result += ")"
@@ -129,16 +154,16 @@ class TupleModelHash(object):
         for e in self.T:
             T[e] = frozenset(perm[i] for i in self.T[e])
 
-        return TupleModelHash(self.model, permute(self.generators, perm), TH=(T, H))
+        return TupleModelHash(self.model, permute(self.generator_tuple, perm), th=(T, H))
 
 
 if __name__ == "__main__":
     from parser import parser
-    model = parser("./malvada.model")
-    ta = [2, 3]
-    tb = [3, 2]
-    fa = TupleModelHash(model, ta)
-    fb = TupleModelHash(model, tb)  # .hit_p([1,0])
-    print(fa)
-    print(fb)
-    print(fa == fb)
+    MODEL = parser("./malvada.model")
+    TA = [2, 3]
+    TB = [3, 2]
+    FA = TupleModelHash(MODEL, TA)
+    FB = TupleModelHash(MODEL, TB)  # .hit_p([1,0])
+    print(FA)
+    print(FB)
+    print(FA == FB)
