c18bd7b52e8691ba0a269373fae5c7ea6828158e
Pablo Ventura <pablogventura@gmail.com>
2019-04-05 11:46:33 -0300

    tests

--- tests.py
+++ tests.py
@@ -1,7 +1,10 @@
+"""
+Toma como argumento el directorio donde estan los modelos
+"""
+
 import os
 for f in  [os.path.join(dp, f) for dp, dn, fn in os.walk("testing") for f in fn]:
     if f.endswith(".model") and not os.path.exists(f.replace(".model",".hvm")):
-        os.system("python3 test_minion_vs_hit.py < %s > %s" % (f,f.replace(".model",".hvm")))
-        
+        os.system('python3 test_minion_vs_hit.py "%s" > "%s"' % (f,f.replace(".model",".hvm")))
     
     
