bef4f575682e81b7530c4d2789443917b56f1a1d
Pablo Ventura <pablo@alberto.WORKGROUP>
2019-04-05 16:14:00 -0300

    porcentaje

--- tests.py
+++ tests.py
@@ -3,8 +3,10 @@ Toma como argumento el directorio donde estan los modelos
 """
 
 import os
-for f in  [os.path.join(dp, f) for dp, dn, fn in os.walk("testing") for f in fn]:
+files = [os.path.join(dp, f) for dp, dn, fn in os.walk("testing") for f in fn]
+for i,f in enumerate(files):
     if f.endswith(".model") and not os.path.exists(f.replace(".model",".hvm")):
+        print("%s%%" % (i / len(files)))
         os.system('python3 test_minion_vs_hit.py "%s" > "%s"' % (f,f.replace(".model",".hvm")))
     
     
