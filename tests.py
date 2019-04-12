c37a2ec8198ec2bea8af30a9877b31c4101eca97
Pablo Ventura <pablogventura@gmail.com>
2019-04-12 11:39:01 -0300

    try

--- tests.py
+++ tests.py
@@ -1,12 +1,13 @@
 """
 Toma como argumento el directorio donde estan los modelos
 """
-
-import os
-files = [os.path.join(dp, f) for dp, dn, fn in os.walk("testing") for f in fn]
-for i,f in enumerate(files):
-    if f.endswith(".model") and not os.path.exists(f.replace(".model",".hvm")):
-        print("%s%%" % (i / len(files)))
-        os.system('python3 test_minion_vs_hit_multiple.py "%s" > "%s"' % (f,f.replace(".model",".hvm")))
-    
+try:
+    import os
+    files = [os.path.join(dp, f) for dp, dn, fn in os.walk("testing") for f in fn]
+    for i,f in enumerate(files):
+        if f.endswith(".model") and not os.path.exists(f.replace(".model",".hvm")):
+            print("%s%%" % (i / len(files)))
+            os.system('python3 test_minion_vs_hit_multiple.py "%s" > "%s"' % (f,f.replace(".model",".hvm")))
+except KeyboardInterrupt:
+    pass
     
