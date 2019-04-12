318f325f5d9f4cafa44b608fc09be8591d6fa64a
Pablo Ventura <pablogventura@gmail.com>
2019-04-12 14:09:19 -0300

    tests

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ Toma como argumento el directorio donde estan los modelos
 """
 from shell_non_blocking import ShellProc
 from time import sleep
-cores = 6
+cores = 5
 procs = []
 try:
     import os
@@ -11,10 +11,9 @@ try:
     for i,f in enumerate(files):
         if f.endswith(".model") and not os.path.exists(f.replace(".model",".hvm")):
             print("%s%%" % (i / len(files)))
-            
-            procs = [p for p in procs if p.is_running()]
-            if len(procs) < cores:
-                procs.append(ShellProc('python3 test_minion_vs_hit_multiple_times.py "%s" > "%s"' % (f,f.replace(".model",".hvm"))))
+            while len(procs) >= cores:
+                procs = [p for p in procs if p.is_running()]
+            procs.append(ShellProc('python3 test_minion_vs_hit_multiple_times.py "%s" > "%s"' % (f,f.replace(".model",".hvm"))))
             
 except KeyboardInterrupt:
     pass
