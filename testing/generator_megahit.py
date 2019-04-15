c629f4102de8406375321a3897eee814e5e0fd6d
Pablo Ventura <pablo@alberto.WORKGROUP>
2019-04-15 18:08:55 -0300

    genreador megahit

--- testing/generator_megahit.py
+++ testing/generator_megahit.py
@@ -1,12 +1,12 @@
 
 import os
 from random import randint
-
+path_mega_hit = "mega_hit_test"
 def generar(*args, cuantity=100):
     # genera alg random
     args = [str(i) for i in args]
     for i in range(cuantity):
-        filename = os.path.join(args[0], '_'.join(args[1:] + [str(i)]))
+        filename = os.path.join(path_mega_hit, args[0], '_'.join(args[1:] + [str(i)]))
         filename += ".model"
         if os.path.isfile(filename):
             continue
@@ -22,21 +22,8 @@ def generar(*args, cuantity=100):
 
         os.system("python3 " + script + " " + argumentos + " > " + filename)
 
-generar("grupo_abeliano",2,3,4)
-for i in range(3):
-    generar("grupo_abeliano",randint(2,5),randint(2,5),randint(2,5))
-
-generar("grupo_abeliano",2,3,4)
-generar("grupo_no_abeliano",3,2)
-for i in range(3):
-    generar("grupo_no_abeliano",randint(2,5),2)
-generar("boole",3)
-generar("boole",7)
-generar("ret",4,4)
-for i in range(3):
-    generar("ret",randint(2,5),randint(2,5))
-generar("alg_random",10,2,2,[2,3],[False,True],3)
-generar("alg_random",20,2,2,[2,3],[False,True],3)
-generar("alg_random",30,3,3,[2,3],[False,True],3)
+generar("boole",4)
+generar("grupo_abeliano",2,2,4)
+generar("alg_random",16,0,0,[2,2],[False,False],2)
 
 
