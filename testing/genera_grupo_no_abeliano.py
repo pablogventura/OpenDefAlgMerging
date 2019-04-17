4df4d4bf1aafbba856d130a13cb0dfcee3897643
Pablo Ventura <pablogventura@gmail.com>
2019-04-16 22:15:08 -0300

    no abelianos de tamano arbitrario

--- testing/genera_grupo_no_abeliano.py
+++ testing/genera_grupo_no_abeliano.py
@@ -20,16 +20,17 @@ def compose(a,b):
         r.append(b[a[i]])
     return tuple(r)
 
-def generador(perm, gens):
+def generador(perm, gens, q=None):
     """
     :param perm: toma el tamanno del del que van a ser las permutaciones
     :param gens: cantidad de generadores aleatorios
     """
     permutaciones = set(permutations(range(perm)))
     print("# Cantidad de permutaciones posibles: %s" % len(permutaciones))
-    universe = set(sample(permutaciones,gens))
-
+    
     sigo=True
+    universe = set(sample(permutaciones, gens))
+    universe.add(tuple(range(perm)))
     while sigo:
         sigo = False
         for a,b in product(universe,universe):
@@ -38,7 +39,10 @@ def generador(perm, gens):
             if r not in universe:
                 universe.add(r)
                 sigo = True
-    universe.add(tuple(range(perm)))
+        if (q and (len(universe) > q) or (q and (not sigo and len(universe)<q))):
+            universe = set(sample(permutaciones, gens))
+            universe.add(tuple(range(perm)))
+            sigo = True
     universe = sorted(universe)
     print("# Quedaron: %s" % len(universe))
     print(" ".join(str(i) for i in range(len(universe))))
@@ -62,12 +66,15 @@ def generador(perm, gens):
 
 def main():
     try:
-        perm, gens = [int(i) for i in sys.argv[1:]]
+        perm, gens = [int(i) for i in sys.argv[1:3]]
     except:
         print("Toma los numeros de los Z")
         sys.exit(1)
-
-    generador(perm, gens)
+    try:
+        q = int(sys.argv[3])
+    except:
+        q=None
+    generador(perm, gens, q)
 
 
 if __name__ == "__main__":
